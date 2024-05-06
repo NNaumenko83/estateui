ef203ea0c5a086baf68030ef19a5119a2ecfeae3
Mykola <nnv2006@ukr.net>
2024-05-06 15:43:57 +0300

    ADD: HomePage

---
+++ src/pages/homePage/HomePage.jsx
@@ -0,0 +1,14 @@
+import './HomePage.scss';
+
+const HomePage = () => {
+    return (
+        <div className="homePage">
+            <div className="textContainer">text</div>
+            <div className="imgContainer">
+                <img src="/bg.png" alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default HomePage;
