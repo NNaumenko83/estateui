9a9fca43ea09f3ca28e620a94d7868b3e5ee266a
Mykola <nnv2006@ukr.net>
2024-05-07 11:11:44 +0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,7 @@
-import Navbar from './components/Navbar/Navbar';
 import './layout.scss';
-import HomePage from './pages/homePage/HomePage';
 
 function App() {
-    return (
-        <div className="layout">
-            <div className="navbar">
-                <Navbar />
-            </div>
-            <div className="content">
-                <HomePage />
-            </div>
-        </div>
-    );
+    return <div className=""></div>;
 }
 
 export default App;
